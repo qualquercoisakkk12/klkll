28deb7e85f1d7ca43858d80668aeaea4304b7bd5
qualquercoisakkk12 <144022313+qualquercoisakkk12@users.noreply.github.com>
2023-10-02 09:14:14 -0300

    adicionando js css

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function draw() {
+    background(220);
+    fill("black");
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    let palavra = "caminhante";
+    let maximo = width;
+    let minimo = I;
+    let quantidade = map(mouseX, 0, maximo, minimo, palavra.length);
+  if(mouseX < 50){         
+    let palavra = "C";
+    text(palavra, 200, 200);
+  } else if (mouseX < 100)}
+  let palavra = "Ca";
+  text(palavra, 200, 200);
+  }else {  
+    let palavra = "caminhante";
+  
+    
+  let(parcial = palavra.substring(0,quantidade);
+    text(palavra, 200, 200);
+  }function setup() {
+    createCanvas(400, 400);
+  }
